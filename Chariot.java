07d16d94f610d480aa6c6786fcbe2e58f7076acb
jeevanxworkz <jeevan.xworkz@gmail.com>
2023-07-23 10:17:05 -0700

    enter any message

---
+++ Chariot.java
@@ -0,0 +1,89 @@
+class Chariot{
+	
+	String components;
+	String dimensions;
+	int wheelSize;
+	String types;
+	int roundPenSize;
+	int sizeOfHorse;
+	int weigh;
+	String color;
+	int wheels;
+	
+	
+	Chariot(String components)
+	{
+		this.components=components;
+	}
+	Chariot(String components,String dimensions)
+	{
+		this.components=components;
+		this.dimensions=dimensions;
+	}
+	Chariot(String components,String dimensions,int wheelSize)
+	{
+		this.components=components;
+		this.dimensions=dimensions;
+		this.wheelSize=wheelSize;
+	}
+	Chariot(String components,String dimensions,int wheelSize,String types)
+	{
+		this.components=components;
+		this.dimensions=dimensions;
+		this.wheelSize=wheelSize;
+		this.types=types;
+	}
+	Chariot(String components,String dimensions,int wheelSize,String types,int roundPenSize)
+	{
+		this.components=components;
+		this.dimensions=dimensions;
+		this.wheelSize=wheelSize;
+		this.types=types;
+		this.roundPenSize=roundPenSize;
+	}
+	Chariot(String components,String dimensions,int wheelSize,String types,int roundPenSize,int sizeOfHorse)
+	{
+		this.components=components;
+		this.dimensions=dimensions;
+		this.wheelSize=wheelSize;
+		this.types=types;
+		this.roundPenSize=roundPenSize;
+		this.sizeOfHorse=sizeOfHorse;
+	}
+	Chariot(String components,String dimensions,int wheelSize,String types,int roundPenSize,int sizeOfHorse,int weigh)
+	{
+		this.components=components;
+		this.dimensions=dimensions;
+		this.wheelSize=wheelSize;
+		this.types=types;
+		this.roundPenSize=roundPenSize;
+		this.sizeOfHorse=sizeOfHorse;
+		this.weigh=weigh;
+	}
+	Chariot(String components,String dimensions,int wheelSize,String types,int roundPenSize,int sizeOfHorse,int weigh,String color)
+	{
+		this.components=components;
+		this.dimensions=dimensions;
+		this.wheelSize=wheelSize;
+		this.types=types;
+		this.roundPenSize=roundPenSize;
+		this.sizeOfHorse=sizeOfHorse;
+		
+	}
+	Chariot(String components,String dimensions,int wheelSize,String types,int roundPenSize,int sizeOfHorse,int weigh,String color,int wheels)
+	{
+		this.components=components;
+		this.dimensions=dimensions;
+		this.wheelSize=wheelSize;
+		this.types=types;
+		this.roundPenSize=roundPenSize;
+		this.sizeOfHorse=sizeOfHorse;
+		this.weigh=weigh;
+		this.color=color;
+		this.wheels=wheels;
+	}
+	
+	
+	
+	
+}
